abd8e355d97a142dabb3536bf27b5d698c3b2f90
k4350 <kaushik4350@gmail.com>
2024-04-23 19:26:20 +0530

    first

---
+++ first.js
@@ -0,0 +1,12 @@
+const employee = {
+    fullName : "Kaushik Baruah",
+    id : 26190072,
+    role : 'Developer',
+    designation : 'DM',
+    isDev : true,
+};
+
+
+employee.role = "senior Dev";
+
+console.log(employee.role);
