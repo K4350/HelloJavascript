1e0f386967472f8949a418178874ffdadd1f496b
k4350 <kaushik4350@gmail.com>
2024-05-01 17:32:12 +0530

    arrays

---
+++ arrays/script.js
@@ -0,0 +1,45 @@
+// let pen = {
+//     name : "ball pen",
+//     price : 10,
+// }
+
+
+// let output = `the price of the ${pen.name} pen is ${pen.price} rupees.`;
+
+
+// console.log(output);
+
+
+// let arr = [23,34,23,34,343,343];
+
+// let arr2 = ["kaushik", "Bonti"];
+
+// let sum = 0;
+
+
+// for(let marks of arr){
+//     sum = sum + marks;
+// }
+// let avg = sum / arr.length;
+
+// alert(`${arr2.charAt(1)} the avg is:  ${avg}`);
+
+
+
+
+let companies = ["Bloomberg", "Microsoft", "Uber", "Google", "IBM", "Netflix"];
+
+// Delete the first company
+// companies.shift();
+// console.log(companies);
+
+
+// delete uber and place Ola
+// companies.splice(2,1,"Ola");
+// console.log(companies);
+
+
+// add amazon at the end
+
+companies.push("Amazon");
+console.log(companies);
