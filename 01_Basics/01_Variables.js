498df69ab59f9d048bccfac393757b30a0a22836
k4350 <kaushik4350@gmail.com>
2024-11-03 11:22:14 +0530

    Basics/varible

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,9 @@
+const accountID = 113343
+let accountEmail = "kaushik@google.com"
+var accountPassword = "3424"
+accountCity = "Pune"
+
+console.log(accountID);
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity])
