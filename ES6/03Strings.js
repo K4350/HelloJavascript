cf616bdbdd859ecc6826dc17d4d09a42ec79cdd1
k4350 <kaushik4350@gmail.com>
2025-02-09 17:24:51 +0530

    Strings

--- ES6/03Strings.js
+++ ES6/03Strings.js
@@ -1,11 +1,28 @@
-let reverseString = name => {
+//Reverse A string using ES6 Arrow Function
+
+// let reverseString = name => {
+//     let str = "";
+//     for(let i = name.length - 1; i >= 0; i--){
+//         str = str + name[i];
+//     }
+//     return str;
+// }
+
+// let name = "Kaushik Baruah";
+// console.log(`The reverse of ${name} is ${reverseString(name)}`);
+
+
+
+// Palindrome String using ES6 Arrow Function
+
+let isPalindrome = name => {
     let str = "";
     for(let i = name.length - 1; i >= 0; i--){
         str = str + name[i];
     }
-    return str;
+
+    return name ===str ? "palindrome" : "NotPalindrome";
 }
 
-let name = "Kaushik Baruah";
-
-console.log(`The reverse of ${name} is ${reverseString(name)}`);
+let name = "madam";
+console.log(`The ${name} is a ${isPalindrome(name)} word`);
